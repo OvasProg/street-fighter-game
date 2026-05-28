280fe882da0def0bef700354b0de0bbf28b13ed7
OvasProg <alex.vasyliev.prog@gmail.com>
2026-05-28 23:50:29 +0300

    + implement fighter details fetching with memory cache

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #detailsCache = new Map();
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -13,8 +15,20 @@ class FighterService {
     }
 
     async getFighterDetails(id) {
-        // todo: implement this method
-        // endpoint - `details/fighter/${id}.json`;
+        try {
+            if (this.#detailsCache.has(id)) {
+                return this.#detailsCache.get(id);
+            }
+
+            const endpoint = `details/fighter/${id}.json`;
+            const apiResult = await callApi(endpoint);
+
+            this.#detailsCache.set(id, apiResult);
+
+            return apiResult;
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
